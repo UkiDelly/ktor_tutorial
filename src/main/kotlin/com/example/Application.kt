634c57d1f46c7c5f10cc617a7bbb18e6362d0b5b
Dae Hyeon <daehyeon1999@naver.com>
2023-05-14 18:53:54 +0900

    서버 실행
    - ebmeddedServer 설명
    - Application.module()설명
    - Application.configureRouting() 설명

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,12 +4,34 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
+    
+    
+    /**
+     *  [Netty] - Netty 서버 엔진을 사용하는 것을 의미
+     *  [port] - 서버가 사용할 포트를 지정
+     *  [host] - 서버가 사용할 호스트를 지정
+     *  [module] - 서버가 사용할 모듈을 지정
+     *  [start] - 서버를 시작
+     *  [wait] - 서버가 시작되고 난 후에 블로킹을 할지 여부를 지정
+     *  [embeddedServer] - 서버를 생성
+     *  Application::module의 의미는 module인자에 Application의 module 함수의 참조를 넘겨준다는 의미
+     */
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
-fun Application.module() {
+
+/**
+ * 서버가 사용할 모듈을 지정하는 등록하는 함수, [Application]의 확장함수
+ *
+ */
+fun Application.module(): Unit {
+    
+    // [Application.configureRouting] 함수를 호출
     configureRouting()
 }
+
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,6 +4,10 @@ import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 
+
+/**
+ *  [Application] 클래스의 확장 함수로, routing을 설정하는 함수
+ */
 fun Application.configureRouting() {
     routing {
         get("/") {
